d58e844e20ee47a9e80f1d4091475baea65f2bc9
Moi-meme <maximouth@localhost.localdomain>
2015-03-24 07:12:45 +0100

    pas grand chose

--- puzzle.ml
+++ puzzle.ml
@@ -1,1 +1,2 @@
-
+let split (s : string) : string list =
+  
--- puzzle.mli
+++ puzzle.mli
@@ -17,8 +17,23 @@ type cell =
 | Cell of (bool*color)
 ;;
 
-type map = {ligne : int;
-	    col : int;
-	    map : cell list}
+type map = {
+  ligne : int;
+  col : int;
+  map : cell list}
 ;;
 
+type t = {
+  nom : string;
+  cdep : int;
+  ldep : int;
+  direction : direction;
+  f1 : int list;
+  f2 : int list;
+  f3 : int list;
+  f4 : int list;
+  f5 : int list;
+  map : map;
+};;
+
+val split : string -> string list
