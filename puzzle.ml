0f48ded76ce2bcc33debb287f08ca1d60b79c579
Moi-meme <maximouth@localhost.localdomain>
2015-03-25 12:28:53 +0100

    type dans le ml

--- puzzle.ml
+++ puzzle.ml
@@ -1,2 +1,45 @@
+(***** declaration des types   *****)
+
+type direction =
+  | North
+  | South
+  | Est
+  | West
+;;
+
+type color =
+  | Empty
+  | Red
+  | Blue
+  | Green
+  | Yellow
+;;
+
+type cell =
+| Cell of (bool*color)
+;;
+
+type map = {
+  ligne : int;
+  col : int; 
+  map : cell list}
+;;
+
+type t = {
+  nom : string;
+  cdep : int;
+  ldep : int;
+  direction : direction;
+  f1 : int list;
+  f2 : int list;
+  f3 : int list;
+  f4 : int list;
+  f5 : int list;
+  map : map;
+};;
+
+(*****  fin de declaration des types  *****)
+
+
 let split (s : string) : string list =
   []
